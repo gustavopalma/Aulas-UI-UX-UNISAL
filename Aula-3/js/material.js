46c0760a6e66c725c7301f358525c6aecb4e71ed
Gustavo Ferreira Palma <gustavofpalma@gmail.com>
2026-08-21 18:42:53 -0300

    Adicionando Aula 3, com conteúdos sobre Material UI

---
+++ Aula-3/js/material.js
@@ -0,0 +1,23 @@
+import '@material/web/button/filled-button.js';
+import '@material/web/button/filled-tonal-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/button/text-button.js';
+
+import '@material/web/checkbox/checkbox.js';
+import '@material/web/radio/radio.js';
+import '@material/web/switch/switch.js';
+
+import '@material/web/textfield/filled-text-field.js';
+import '@material/web/textfield/outlined-text-field.js';
+
+import '@material/web/iconbutton/icon-button.js';
+import '@material/web/icon/icon.js';
+
+import '@material/web/fab/fab.js';
+
+import '@material/web/dialog/dialog.js';
+
+import '@material/web/progress/circular-progress.js';
+import '@material/web/progress/linear-progress.js';
+
+import '@material/web/chips/filter-chip.js';
